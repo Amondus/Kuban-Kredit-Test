5664619688b55edc0e61829f4f991b6c2eae2563
Amondus <50753491+Amondus@users.noreply.github.com>
2019-12-20 10:34:40 +0300

    Initial Commit

---
+++ Kuban Kredit Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kuban Kredit Test
+//
+//  Created by Антон Захарченко on 20.12.2019.
+//  Copyright © 2019 Антон Захарченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
